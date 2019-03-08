22419f4c925849920896765091c7cdcd731d7c4b
Yossi Mosbacher <joe.mosbacher@gmail.com>
2019-03-08 09:56:41 +0200

    restore cs files

---
+++ instrosetta-csharp/instrosetta/servers/motion/linear/singleaxis/ThorlabsKDC101Server/SingleLinearAxisImpl.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+using Thorlabs.MotionControl.DeviceManagerCLI;
+using Thorlabs.MotionControl.KCube.DCServoCLI;
+using Thorlabs.MotionControl.GenericMotorCLI;
+
+using Grpc.Core;
+using Grpc.Core.Utils;
+using UnitsNet;
+
+namespace Devices.Motion.Linear.Singleaxis
+{
+    class SingleLinearAxisImpl : SingleLinearAxis.SingleLinearAxisBase
+
+    {
+        private KDC101 _Motor = new KDC101();
+        private UnitParser _uparser = UnitParser.Default; 
+        
+        public override Task<Device> Connect(ConnectRequest request, ServerCallContext context)
+        {
+            if (_Motor != null)
+            {
+                if (_Motor.SerialNo == request.Device.SerialNumber.ToString())
+                {
+                    return Task.FromResult(request.Device);
+                }
+                else
+                {
+                    try
+                    {
+                        _Motor.Disconnect();
+           
+                    }
+                    catch (Exception ex)
+                    {
+                        Status stat = new Status(StatusCode.Unavailable, "Failed to disconnect from current device." + ex.Message);
+                        Metadata meta = new Metadata
+                        {
+                            { "exception_name", ex.GetType().Name }
+                        };
+                        throw new RpcException(stat, meta);
+                    }
+
+                }
+
+            }
+
+            int timeout = (int) TimeSpan.FromSeconds(request.Timeout).TotalMilliseconds;
+            int interval = (int) TimeSpan.FromSeconds(request.PollingInterval).TotalMilliseconds;
+            try
+            {
+                _Motor.Connect(request.Device.SerialNumber.ToString(), timeout, interval);
+            }
+            catch (Exception ex)
+            {
+                Status stat = new Status(StatusCode.NotFound, "Failed to connect to device." + ex.Message);
+                Metadata meta = new Metadata
+                        {
+                            { "exception_name", ex.GetType().Name }
+                        };
+                throw new RpcException(stat, meta);
+            }
+            
+            return Task.FromResult(request.Device);
+
+        }
+
+        public Task<Device> Disconnect()
+        {
+            Device dev = new Device { SerialNumber = UInt32.Parse(_Motor.SerialNo) };
+
+            try
+            {
+                _Motor.Disconnect();
+                
+            }
+            catch (Exception ex)
+            {
+                Status stat = new Status(StatusCode.NotFound, "Failed to disconnect from device." + ex.Message);
+                Metadata meta = new Metadata
+                        {
+                            { "exception_name", ex.GetType().Name }
+                        };
+                throw new RpcException(stat, meta);
+            }
+
+            return Task.FromResult(dev);
+
+        }
+
+
+        public override async Task MoveAbsolute(MoveAbsoluteRequest request, IServerStreamWriter<Position> responseStream, ServerCallContext context)
+        {
+            
+            decimal destination = (decimal) Length.Parse(String.Format("{0} {1}", request.Position.Value, request.Position.Units)).Millimeters;
+
+            foreach (long position in _Motor.MoveAbsolute(destination)) {
+                Position response = new Position
+                {
+                    Value = position,
+                    Units = "mm"
+
+                };
+                await responseStream.WriteAsync(response);
+            }
+
+ 
+
+        }
+
+        public override async Task MoveRelative(MoveRelativeRequest request, IServerStreamWriter<Position> responseStream, ServerCallContext context)
+        {
+
+            decimal distance = (decimal)Length.Parse(String.Format("{0} {1}", request.Distance.Value, request.Distance.Units)).Millimeters;
+
+            foreach (long position in _Motor.MoveRelative(distance, request.Distance.Direction))
+            {
+                Position response = new Position
+                {
+                    Value = position,
+                    Units = "mm"
+
+                };
+                await responseStream.WriteAsync(response);
+            }
+
+
+
+        }
+
+    }
+
+
+}
