e09afd461307263b6154c675b1c2fbb43f5ca14b
Yossi Mosbacher <joe.mosbacher@gmail.com>
2019-04-12 09:52:16 +0300

    update spectrograpf interface

--- instrosetta-servers/python/andor/solis_proxy/solis_proxy_device.py
+++ instrosetta-servers/python/andor/solis_proxy/solis_proxy_device.py
@@ -5,26 +5,29 @@ import ast
 import enum
 
 
-class ShutterState(enum.Enum):
-    CLOSED = 0
-    OPEN = 1
-    AUTO = 2
+def test_connected(f):
+    def wrapped(self, *args, **kwargs):
+        if not self.connected:
+            raise ConnectionError("Device not connected.")
+        return f(self, *args, **kwargs)
+    return wrapped
 
+class TestableEnum(enum.Enum):
     @classmethod
     def has_value(cls, value):
         return any(value == item.value for item in cls)
 
+class ShutterState(TestableEnum):
+    CLOSED = 0
+    OPEN = 1
+    AUTO = 2
 
-class ReadoutMode(enum.Enum):
+class ReadoutMode(TestableEnum):
     FVB = 0
     MULTI_TRACK = 1
     RESERVED = 2
     SINGLE_TRACK = 3
     IMAGE = 4
-
-    @classmethod
-    def has_value(cls, value):
-        return any(value == item.value for item in cls)
 
 
 class SolisProxy:
@@ -37,57 +40,72 @@ class SolisProxy:
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.conn = None
+        self.sio = None
+        self.polling_interval = 0.2
 
+    @test_connected
     def read(self):
-        msg = self.conn.read(200).strip().decode()
+        msg = self.sio.readline().strip()
         self.conn.write(f"ACK:{msg}")
         return msg
 
+    @test_connected
     def write(self, msg):
-        self.conn.write(f'{msg}\r'.encode())
+        self.sio.flush()
+        self.sio.write(f'{msg}')
         time.sleep(0.1)
-        resp = self.conn.read(200).strip().decode()
+        resp = self.sio.readline().strip()
         if resp != f"ACK:{msg}":
             raise ConnectionError("Message not acknowledged by peer.")
         return True
 
-    def query(self, msg):
-        self.write(msg)
-        resp = self.read()
-        term = self.read()
-        if term != "OK":
+    def read_response(self, timeout):
+        responses = []
+        start = time.time()
+        while abs(time.time()-start)<timeout:
+            resp = self.read()
+            if resp == "OK":
+                break
+            elif resp != "":
+                responses.append(resp)
+            else:
+                time.sleep(self.polling_interval)
+        else:
             raise ConnectionError("Conversation not terminated by peer.")
+        if len(responses)==1:
+            return responses[0]
+        else:
+            return responses
+
+    def query(self, *msgs, timeout=1):
+        for msg in msgs:
+            self.write(msg)
+        resp = self.read_response(timeout)
         return resp
-
-    def command(self, cmd, *args):
-        self.write(cmd)
-        for arg in args:
-            self.write(arg)
-
+        
     def save(self, path):
-        self.command("CALL:SAVE", path)
+        self.query("CALL","SAVE", path)
 
     def clear_screen(self):
-        self.command("CALL:CLEARSCREEN")
+        self.query("CALL","CLEARSCREEN")
         
     def get_shutter_state(self):
-        return self.query("GET:SHUTTER")
+        return self.query("GET","SHUTTER")
 
     def set_shutter_state(self, state: ShutterState):
-
-        self.command("SET:SHUTTER", state.value)
+        self.query("SET","SHUTTER", state.value)
 
     def run(self):
-        self.query('CALL:RUN')
+        self.query('CALL","RUN')
 
     @property
     def grating(self):
-        return self.query("GET:GRATING")
+        return self.query("GET","GRATING")
 
     @grating.setter
     def grating(self, value):
         if value in [1, 2]:
-            self.command("SET:GRATING", value)
+            self.query("SET","GRATING", value)
         while True:
             if self.grating == f"{value}":
                 break
@@ -96,34 +114,36 @@ class SolisProxy:
 
     @property
     def wavelength(self):
-        return self.query("GET:WAVELENGTH")
+        return self.query("GET","WAVELENGTH")
 
     @wavelength.setter
     def wavelength(self, value):
         if (2000 >= value >= 200):
-            self.command("SET:WAVELENGTH", value)
+            self.query("SET","WAVELENGTH", value)
 
     @property
     def exposure(self):
-        return self.query("GET:EXPOSURE")
+        return self.query("GET","EXPOSURE")
 
     @exposure.setter
     def exposure(self, value):
-        self.command("SET:EXPOSURE", value)
+        self.query("SET","EXPOSURE", value)
 
     @property
     def slit_width(self):
-        return self.query("GET:SLIT_WIDTH")
+        return self.query("GET","SLIT_WIDTH")
 
     @slit_width.setter
     def slit_width(self, value):
         if (2500 >= value >=10):
-            self.command("SET:SLIT_WIDTH", value)
+            self.query("SET","SLIT_WIDTH", value)
 
     def connect(self, com_port, baudrate=115200, timeout=0.2):
+        self.polling_interval = timeout
         self.disconnect()
         try:
             self.conn = serial.Serial(com_port, baudrate=baudrate, timeout=timeout)
+            self.sio = io.TextIOWrapper(io.BufferedRWPair(self.conn, self.conn))
         except:
             pass
 
